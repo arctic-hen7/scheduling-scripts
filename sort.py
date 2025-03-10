34420d17cdaafa59335c4020cf9db21dd11d1b49
arctic-hen7 <arctic.hen@pm.me>
2025-03-09 22:04:32 -0700

    refactor: improved sorting order

--- sort.py
+++ sort.py
@@ -1,11 +1,18 @@
+from datetime import datetime, timedelta
+
 def sort_actions(actions):
+    critical_cutoff = datetime.now() + timedelta(days=1)
+    critical_cutoff.replace(hour=23, minute=59, second=59)
+    critical_cutoff = datetime.strftime(critical_cutoff, "%Y-%m-%d")
+
     actions.sort(
         key=lambda item:
             (
-                item["scheduled"]["date"] if item["scheduled"] else item["deadline"]["date"] if item["deadline"] else "9999",
-                item["scheduled"]["time"] if item["scheduled"] else item["deadline"]["time"] if item["deadline"] else "9999",
                 item["deadline"]["date"] if item["deadline"] else "9999",
                 item["deadline"]["time"] if item["deadline"] else "9999",
+                item["scheduled"]["date"] if item["scheduled"] else "9999",
+                item["scheduled"]["time"] if item["scheduled"] else "9999",
+                item["priority"], # Lower is better
                 item["title"]
             )
     )
